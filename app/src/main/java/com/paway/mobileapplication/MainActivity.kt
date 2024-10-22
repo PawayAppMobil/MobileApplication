f1b706b7e4211ba1f75fa21158db8f3149f4c59d
Arturo Adrianzén <cartureto@gmail.com>
2024-10-22 13:09:18 -0500

    feat: updated main activity

--- app/src/main/java/com/paway/mobileapplication/MainActivity.kt
+++ app/src/main/java/com/paway/mobileapplication/MainActivity.kt
@@ -1,30 +1,46 @@
 package com.paway.mobileapplication
 
 import InvoiceDashboardScreen
+import com.paway.mobileapplication.data.repository.report.ReportRepository
+import ReportScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.List
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.paway.mobileapplication.data.repository.WebServiceRepository
 import com.paway.mobileapplication.common.RetrofitClient
+import com.paway.mobileapplication.data.repository.WebServiceRepository
 import com.paway.mobileapplication.presentation.facturas.InvoiceDashboardViewModel
+import com.paway.mobileapplication.presentation.reports.ReportViewModel
 import com.paway.mobileapplication.ui.theme.MobileApplicationTheme
+
 val InvoiceDashboardviewModel = InvoiceDashboardViewModel(WebServiceRepository(RetrofitClient.webService))
+val ReportViewModel = ReportViewModel(ReportRepository(RetrofitClient.reportService))
 
 class MainActivitySelector : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -82,7 +98,7 @@ fun InvoiceScreen() {
 
 @Composable
 fun BalanceScreen() {
-    Text(text = "Pantalla de Balance General")
+    ReportScreen(reportViewModel = ReportViewModel)
 }
 
 @Composable
