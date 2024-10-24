f701755f85eefa33f8bc4023918b44b07808607c
Sergio Pecan <u20201a938@upc.edu.pe>
2024-10-23 19:13:46 -0500

    update

--- app/src/main/java/com/paway/mobileapplication/invoces/domain/model/invoice/InvoiceItem.kt
+++
@@ -1,9 +0,0 @@
-package com.paway.mobileapplication.invoces.domain.model.invoice
-
-data class InvoiceItem(
-    val id: String,
-    val description: String,
-    val quantity: Int,
-    val unitPrice: Double,
-    val productId: String
-)
