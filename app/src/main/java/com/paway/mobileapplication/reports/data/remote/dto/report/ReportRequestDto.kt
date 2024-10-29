b10bd41a5a8c25720a85ce93999c3a48efb5d9f7
Arturo Adrianzén <cartureto@gmail.com>
2024-10-29 00:21:04 -0500

    feat: updated reportRequest for report post functionality

--- app/src/main/java/com/paway/mobileapplication/reports/data/remote/dto/report/ReportRequestDto.kt
+++ app/src/main/java/com/paway/mobileapplication/reports/data/remote/dto/report/ReportRequestDto.kt
@@ -1,7 +1,8 @@
 package com.paway.mobileapplication.reports.data.remote.dto.report
 
 data class ReportRequestDto(
-    val startDate: String? = null,
-    val endDate: String? = null,
-    val type: String? = null
+    val userId: String,
+    val reportType: String,
+    val dateRange: DateRange
 )
+
