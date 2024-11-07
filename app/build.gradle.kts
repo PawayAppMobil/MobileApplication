4867a356314c50d21e63100a0b57f69ee2ce442b
Arturo Adrianzén <cartureto@gmail.com>
2024-11-07 05:37:49 -0500

    feat: added MPAndroidChart dependencie

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
 
+    implementation ("com.github.PhilJay:MPAndroidChart:v3.1.0")
+
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
     implementation(libs.glide)
